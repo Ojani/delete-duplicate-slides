37d09af31ac2339941f778df650dad1b6720cb36
Ojani <ojanifigueroa.of@gmail.com>
2025-10-02 12:15:57 -0400

    Added number of removed pages to output. Formatted main.py with autoformatter.

--- main.py
+++ main.py
@@ -4,11 +4,13 @@ import pymupdf
 import sys
 import os
 
+
 def printError(msg):
     print("\033[91merror:\033[0m " + msg)
 
+
 def deleteDuplicateSlides(args):
-    filepaths = [str(path) for path in Path(args.filename).rglob('*.pdf')]
+    filepaths = [str(path) for path in Path(args.filename).rglob("*.pdf")]
 
     print(f"Identified {len(filepaths)} PDF{'' if len(filepaths) == 1 else 's'}.\n")
 
@@ -19,18 +21,20 @@ def deleteDuplicateSlides(args):
     for filepath in filepaths:
         outputname = args.outputname
 
-        print(f"Processing \033[4m{os.path.basename(filepath)}\033[0m.\n")
+        print(f"Processing \033[4m{os.path.basename(filepath)}\033[0m...")
 
         # removing duplicates
         doc = pymupdf.open(filepath)
-        prev_text = ''
+        removed = 0
+        prev_text = ""
 
-        for i in range(len(doc)-1, -1, -1):
+        for i in range(len(doc) - 1, -1, -1):
             current_text = doc[i].get_text().encode("utf8")
 
             if current_text == prev_text:
                 doc.delete_page(i)
-            
+                removed += 1
+
             prev_text = current_text
 
         # saving file
@@ -40,35 +44,40 @@ def deleteDuplicateSlides(args):
             outputname = filepath[0:-4] + "-no-duplicates.pdf"
         elif not outputname:
             outputname = filepath[0:-4] + "-no-duplicates.pdf"
-        elif not outputname.endswith('.pdf'):
+        elif not outputname.endswith(".pdf"):
             if len(filepaths) > 1:
-                outputname = filepath + outputname + '.pdf'
+                outputname = filepath + outputname + ".pdf"
             else:
                 outputname += ".pdf"
 
         doc.save(outputname)
-        
+
         # deleting original and renaming new one if replace option was chosen
         if args.replace:
             oldname = outputname
-            new_name = oldname[0:-18] + '.pdf'
+            new_name = oldname[0:-18] + ".pdf"
             outputname = new_name
 
             os.rename(oldname, new_name)
             os.remove(filepath[0:-4] + "-original.pdf")
 
-        print("Saved output to \033[4m" + outputname + "\033[0m.\n")
-   
+        print(
+            f"Saved as \033[4m{os.path.basename(outputname)}\033[0m.\n"
+            + f"Removed {removed} page{'' if removed == 1 else 's'}.\n"
+        )
+
+
 if __name__ == "__main__":
     argparser = argparse.ArgumentParser(
-                    prog='dds (Delete Duplicate Slides)',
-                    description='deletes pdfs with duplicate text content, leaving the most recent page by default.')
-    
-    argparser.add_argument('filename')
-    argparser.add_argument('-o', '--outputname')
+        prog="dds (Delete Duplicate Slides)",
+        description="deletes pdfs with duplicate text content, leaving the most recent page by default.",
+    )
+
+    argparser.add_argument("filename")
+    argparser.add_argument("-o", "--outputname")
     # When can be used no output name is provided so that instead of appending
     # "-no-duplicates" to the output, it replaces the original file
-    argparser.add_argument('-r', '--replace', action='store_true')
+    argparser.add_argument("-r", "--replace", action="store_true")
     args = argparser.parse_args()
 
     args.filename = str(Path(args.filename).resolve())
